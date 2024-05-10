a0f85430d2fb1abecd7688d61bb19065ee04ac5e
Aaron Harbourne <ah@Aarons-MacBook-Pro.local>
2024-05-10 15:47:15 +0100

    Delete models and re-make them due to an error

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Band
 
 # Register your models here.
+class BandAdmin(admin.ModelAdmin):
+    list_display = (
+        'number',
+        'band_name',
+    )
+
+admin.site.register(Band, BandAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Band(models.Model):
+    number = models.IntegerField(primary_key = True, editable = False)
+    band_name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.band_name
+
